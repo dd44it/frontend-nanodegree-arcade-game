a086d920eca8145de221687aaf6010da8c4a1bc7
dd44it <12nelson13@gmail.com>
2022-09-30 21:11:03 +0300

    feat: add movement player and initialization enemies

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
+const config = {
+    player_width: 101,
+    player_heigth: 171,
+    player_move_width: 101,
+    player_move_heigth: 83,
+}
+
 // Enemies our player must avoid
-const Enemy = function() {
+const Enemy = function(x, y, player) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.player = player;
 };
 
 // Update the enemy's position, required method for game
@@ -25,6 +35,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+
 const Player = function(x, y, ){
     this.x = x;
     this.y = y;
@@ -39,17 +50,34 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 const player = new Player(400, 400);
 
-const enemy0 = new Enemy();
-
-const allEnemies = [ enemy0 ]
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const enemy1 = new Enemy(30, 120, 60, 'images/enemy-bug.png', player);
+const enemy2 = new Enemy(15, 220, 30, 'images/enemy-bug.png', player);
+const enemy3 = new Enemy(0, 50, 20, 'images/enemy-bug.png', player);
+const allEnemies = [enemy1, enemy2, enemy3];
 
 
+Player.prototype.handleInput = function(key) {
+    switch(key){
+        case 'left':
+            this.x -= config.player_move_width;
+            break;
+        case 'right':
+            this.x += config.player_move_width;
+            break;
+        case 'up':
+            this.y -= config.player_move_heigth;
+            break;
+        case 'down':
+            this.y += config.player_move_heigth;
+            break;
+    };
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
