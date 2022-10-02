1ae224c5a6fd07d625e25a892aff7ac53a4bbf35
dd44it <12nelson13@gmail.com>
2022-10-02 19:07:24 +0300

    feat: add score counter

--- js/app.js
+++ js/app.js
@@ -7,16 +7,17 @@ const config = {
     map_heigth: 606,
     extreme_point_left: -4,
     extreme_point_top: -15,
-    // find_center_pos_x: function() { return (this.map_width - this.player_width) / 2 },
     start_position_x: 202,
     start_position_y: 400,
     min_speed: 100,
     max_speed: 500,
+    rule_text: 'Your player should reach the water by avoiding bugs.',
+    start_score: 0,
+    win_scrore: 5,
 };
 
+const body = document.querySelector('body');
 
-
-// Enemies our player must avoid
 const Enemy = function(x, y, player) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
@@ -25,14 +26,16 @@ const Enemy = function(x, y, player) {
     this.speed = Math.floor(Math.random() * (config.max_speed - config.min_speed) + config.min_speed);
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
     if (this.x > config.map_width) {
         this.x = 0;
     }
     if(this.checkCollision()){
+        if(this.player.score){
+            this.player.score--;
+            this.player.updateScore(this.player.score);
+        }
         this.player.resetInitialPosition()
     }
 };
@@ -53,10 +56,10 @@ const Player = function(x, y, ){
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
+    this.score = config.start_score;
 };
 
 Player.prototype.update = function () {
-
 
 };
 
@@ -69,7 +72,23 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Player.prototype.showInfoGame = function () {
+    const section = document.createElement('section');
+    section.classList.add('info');
+    section.innerHTML = `   <span>${config.rule_text}</span>
+                            <div class="score_wrapper"> Score: <span> ${ player.score } </span> / 5 </div>
+                        `;
+    body.append(section);
+};
+
+Player.prototype.updateScore = function (currentScore) {
+    const scoreElem = document.querySelector('.score_wrapper span')
+    scoreElem.textContent = currentScore
+    console.log('updateScore')
+}
+
 const player = new Player(config.start_position_x, config.start_position_y);
+player.showInfoGame();
 
 const enemy1 = new Enemy(202, 145, player, );
 const enemy2 = new Enemy(0, 220, player, );
@@ -88,9 +107,23 @@ function checkCollisionsWalls(player){
     }
     else if(player.y < config.extreme_point_top){
         player.y += config.player_move_heigth;
+        return
     }
     if(player.y === config.extreme_point_top){
-        setTimeout(() => { player.resetInitialPosition(); }, 500);
+        setTimeout(() => {
+            player.resetInitialPosition();
+            if(player.score === config.win_scrore){
+                alert('Congratulations, you won. Your score will be reset. You can play again, if you want)')
+                player.score = config.start_score;
+                player.updateScore(player.score);
+            }
+            else {
+                player.score++;
+                player.updateScore(player.score);
+            }
+            
+         }, 500);
+         return
     }
 };
 
@@ -116,8 +149,6 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
